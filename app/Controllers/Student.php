574d26ee2c0a43ca1905d519a19628f812f95e1e
cerkhachacw <19207214@bsi.ac.id>
2022-05-02 20:11:02 +0700

    pertemuan 7, buat form input siswa dan view untuk menampilkan hasil inputan

--- app/Controllers/Student.php
+++ app/Controllers/Student.php
@@ -17,8 +17,6 @@ class Student extends BaseController
 
     public function add_student()
     {
-        // $request = $this->request->getVar();
-        // dd($request['name']);
         return view('view-add-student-data.php', ['data' => $this->request->getVar()]);
     }
 }
